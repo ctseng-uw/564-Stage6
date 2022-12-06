c8ac01ac97cb29eaf5542ad61c56139399551984
WalterLu3 <s10130134@gmail.com>
2022-12-05 18:17:47 -0600

    insert structure

--- insert.C
+++ insert.C
@@ -1,5 +1,10 @@
 #include "catalog.h"
 #include "query.h"
+#include "error.h"
+#include "heapfile.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
 
 /*
  * Inserts a record into the specified relation.
@@ -11,6 +16,64 @@
 
 const Status QU_Insert(const string &relation, const int attrCnt,
                        const attrInfo attrList[]) {
-  // part 6
+  Status status;
+
+  // first check if the number of attribute is correct
+  RelDesc = relDesc;
+  status = relCat->getInfo(relation, relDesc);
+  if (status != OK){
+    return status;
+  }
+  if (relDesc.attrCnt != attrCnt){
+    // need to return an error because NULL is not supported in Minirel
+    return NOTUSED1;
+  }
+
+  // figure out the total length and the order of a record
+  int reclen = 0;
+  int tempAttrCnt = 0;
+  AttrDesc* allAttr;  
+  status = attrCat->getRelInfo(relation, tempAttrCnt, allAttr);
+  if (status != OK){
+    return status;
+  }
+
+  //calculate record length
+  for (int i = 0; i < tempAttrCnt; i++){
+    reclen = reclen + allAttr[i].attrLen;
+  }
+
+  // start the insert scan
+  InsertFileScan insertScan(relation, status);
+  RID rid;
+  if (status != OK){
+    return status;
+  }
+
+  // create dummy record (following from select.C)
+  auto recordWriteBuf = make_unique<char[]>(reclen);
+  recordWrite.length = reclen;
+  recordWrite.data = recordWriteBuf.get();
+
+  // idea : use double loops to put in the attributes in order
+  char *cur = recordWriteBuf.get();
+  for (int i = 0; i < tempAttrCnt; i++){
+    for (int j = 0; j < tempAttrCnt; j++){
+      // if the attrbute names are the same, we can put it in to the record
+      if (strcmp(allAttr[i].attrName, attrList[j].attrName) == 0){
+        memcpy(cur, 
+                static_cast<char *>(attrList[j].attrValue),
+                allAttr[i].attrLen);
+        cur = cur + allAttr[i].attrLen;              
+        break;
+      }
+    }
+  }
+
+  status = insertScan.insertRecord(recordWrite, rid);
+  if (status != OK){
+    return status;
+  }
+
   return OK;
 }
