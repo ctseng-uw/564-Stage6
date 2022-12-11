4321604731cf12581a54cd7c2fe2fb10dacfeb59
chrisknguyen1010 <56803708+chrisknguyen1010@users.noreply.github.com>
2022-12-11 14:26:03 -0600

    Implementation of delete.C (#5)
    * Implementation of delete.C
    
    * Fixed nullptr to null
    
    * Fixed nullptr to null
    
    * Reran clang-format and compiled
    
    * Fixed Null to empty()
    
    * Updated delete.C
    
    * Fixed missing semicolon
    
    * Modernized usage of
    
    * Reran clang-format on delete.C

--- delete.C
+++ delete.C
@@ -1,4 +1,10 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
 #include "catalog.h"
+#include "error.h"
+#include "heapfile.h"
 #include "query.h"
 
 /*
@@ -13,5 +19,83 @@ const Status QU_Delete(const string &relation, const string &attrName,
                        const Operator op, const Datatype type,
                        const char *attrValue) {
   // part 6
+  Status status;
+  RID rid;
+
+  // remove all tuples as no attribute was given
+  if (attrName.empty()) {
+    HeapFileScan scan(relation, status);
+    if (status != OK) {
+      return status;
+    }
+    // start scan with no filter
+    status = scan.startScan(0, relation.length() + 1, STRING, nullptr, EQ);
+    if (status != OK) {
+      return status;
+    }
+
+    // loop through relation and delete all tuples
+    while ((status = scan.scanNext(rid)) != FILEEOF) {
+      status = scan.deleteRecord();
+      if (status != OK) {
+        return status;
+      }
+    }
+    scan.endScan();
+    return OK;
+  }
+
+  // offset calculation
+  int offset = 0;
+  AttrDesc attrDesc;
+  if (!attrName.empty()) {
+    status = attrCat->getInfo(relation, attrName, attrDesc);
+    if (status != OK) {
+      return status;
+    }
+    offset = attrDesc.attrOffset;
+  }
+
+  // filtered HeapFileScan to locate qualifying tuples
+  HeapFileScan scan(relation, status);
+  if (status != OK) {
+    return status;
+  }
+
+  int intFilter;
+  float floatFilter;
+
+  // start scan (similar to select.C)
+  if (type == STRING) {
+    status = scan.startScan(offset, strlen(attrValue), type, attrValue, op);
+    if (status != OK) {
+      return status;
+    }
+  } else if (type == INTEGER) {
+    intFilter = atoi(attrValue);
+    status = scan.startScan(offset, sizeof(int), type,
+                            reinterpret_cast<char *>(&intFilter), op);
+    if (status != OK) {
+      return status;
+    }
+  } else if (type == FLOAT) {
+    floatFilter = atof(attrValue);
+    status = scan.startScan(offset, sizeof(float), type,
+                            reinterpret_cast<char *>(&floatFilter), op);
+    if (status != OK) {
+      return status;
+    }
+  }
+
+  while ((status = scan.scanNext(rid)) != FILEEOF) {
+    // delete tuple
+    status = scan.deleteRecord();
+    if (status != OK) {
+      return status;
+    }
+  }
+
+  // end scan
+  scan.endScan();
   return OK;
 }
